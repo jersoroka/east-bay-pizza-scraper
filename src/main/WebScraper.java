e29144f30de6f6a96f167dd78225a018bd2e3f87
jerso <jersoroka@gmail.com>
2021-05-04 10:37:21 -0700

    parsed out pizza ingredients only

--- src/main/WebScraper.java
+++ src/main/WebScraper.java
@@ -19,9 +19,14 @@ public class WebScraper {
 
             for (Element element: body.select("article")) {
                 final String date = element.select("div.date").select("p").text();
-                final String pizza = element.select("div.menu").text();
+                final String fullPizzaText = element.select("div.menu p").text();
+                final String repeatedPizzaText = element.select("div p b i").text();
+                final String pizzaIngredients =  fullPizzaText.replace(repeatedPizzaText + " ", "");
+
                 System.out.println(date);
-                System.out.println(pizza);
+                //System.out.println(fullPizzaText);
+                //System.out.println(repeatedPizzaText);
+                System.out.println(pizzaIngredients);
             }
         } catch (IOException e) {
             e.printStackTrace();
